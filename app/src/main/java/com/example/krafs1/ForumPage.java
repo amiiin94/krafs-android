251945161f6fd4c7ff15b58374cde8f40bc59066
amiiin94 <amin942004@gmail.com>
2023-11-02 20:29:29 +0700

    nambah page forum

---
+++ app/src/main/java/com/example/krafs1/ForumPage.java
@@ -0,0 +1,14 @@
+package com.example.krafs1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ForumPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.forum_page);
+    }
+}
