e9a4a9f6ef2e47bf263d5c096853d0cbe5b033dc
amiiin94 <amin942004@gmail.com>
2023-11-06 13:25:37 +0700

    nambah forum detail

---
+++ app/src/main/java/com/example/krafs1/ForumDetail.java
@@ -0,0 +1,14 @@
+package com.example.krafs1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ForumDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.forum_detail_page);
+    }
+}
