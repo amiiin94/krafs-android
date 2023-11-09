24ec13abed45a708a58b00f90633d5150480cca3
amiiin94 <amin942004@gmail.com>
2023-11-09 16:33:02 +0700

    buat login jadi constraint

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,3 +47,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
+
+dependencies {
+    implementation("androidx.webkit:webkit:1.8.0")
+}
--- app/src/main/java/com/example/krafs1/ForumPage.java
+++ app/src/main/java/com/example/krafs1/ForumPage.java
@@ -11,4 +11,6 @@ public class ForumPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.forum_page);
     }
+
+
 }
