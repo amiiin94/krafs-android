acc7a8496cb1a0ef46534ee7b13e5ef7e470d9e6
amiiin94 <amin942004@gmail.com>
2023-11-09 20:02:09 +0700

    nambah page profile

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,8 +46,12 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+
+
 }
 
 dependencies {
     implementation("androidx.webkit:webkit:1.8.0")
+    implementation ("com.android.volley:volley:1.1.1")
 }
--- app/src/main/java/com/example/krafs1/LoginPage.java
+++ app/src/main/java/com/example/krafs1/LoginPage.java
@@ -4,11 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginPage extends AppCompatActivity {
     private Button signupBtn;
+    private EditText username;
+    private EditText password;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +26,5 @@ public class LoginPage extends AppCompatActivity {
                 startActivity(signupIntent);
             }
         });
-
     }
 }
