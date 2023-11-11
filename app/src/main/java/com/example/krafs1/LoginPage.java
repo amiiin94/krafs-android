746b589cd69884341c4e0f686c8b9096f3530b6c
amiiin94 <amin942004@gmail.com>
2023-11-11 11:50:58 +0700

    berhasil getUser by email and password dan disimpan dlm shared Preferences

--- app/src/main/java/com/example/krafs1/LoginPage.java
+++ app/src/main/java/com/example/krafs1/LoginPage.java
@@ -1,30 +1,93 @@
 package com.example.krafs1;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.NumberFormat;
+import java.util.Currency;
+import java.util.Locale;
+
 public class LoginPage extends AppCompatActivity {
-    private Button signupBtn;
-    private EditText username;
+    private Button loginBtn;
+    private EditText email;
     private EditText password;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page);
 
-        signupBtn = findViewById(R.id.signupBtn);
+        loginBtn = findViewById(R.id.loginBtn);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
 
-        signupBtn.setOnClickListener(new View.OnClickListener() {
+        loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent signupIntent = new Intent(LoginPage.this, RegisterPage.class);
-                startActivity(signupIntent);
+                login();
             }
         });
+    }
+
+    public void login() {
+        String urlEndPoints = "https://ap-southeast-1.aws.data.mongodb-api.com/app/application-0-iyoxv/endpoint/getUserbyemailandPassword?email=" + email.getText().toString() + "&password=" + password.getText().toString();
+
+        StringRequest sr = new StringRequest(
+                Request.Method.GET,
+                urlEndPoints,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject userJson = new JSONObject(response);
+
+                            String username = userJson.getString("username");
+                            String email = userJson.getString("email");
+                            String notelp = userJson.getString("notelp");
+                            String password = userJson.getString("password");
+
+                            // Store user data in SharedPreferences
+                            SharedPreferences sharedPreferences = getSharedPreferences("UserData", MODE_PRIVATE);
+                            SharedPreferences.Editor editor = sharedPreferences.edit();
+                            editor.putString("username", username);
+                            editor.putString("email", email);
+                            editor.putString("notelp", notelp);
+                            editor.putString("password", password);
+                            editor.apply();
+
+                            Intent profileIntent = new Intent(LoginPage.this, ProfilePage.class);
+                            startActivity(profileIntent);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(LoginPage.this, error.networkResponse.statusCode, Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        requestQueue.add(sr);
     }
 }
--- app/src/main/java/com/example/krafs1/ProfilePage.java
+++ app/src/main/java/com/example/krafs1/ProfilePage.java
@@ -2,13 +2,58 @@ package com.example.krafs1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class ProfilePage extends AppCompatActivity {
+    private LinearLayout homepage, navMerchant, navforum, navarticle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile_page);
+        setContentView(R.layout.profile_page);
+
+        homepage = findViewById(R.id.homepage);
+        navMerchant = findViewById(R.id.navmerchant);
+        navforum = findViewById(R.id.navforum);
+        navarticle = findViewById(R.id.navarticle);
+
+        homepage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent homeIntent = new Intent(ProfilePage.this, MainActivity.class);
+                startActivity(homeIntent);
+            }
+        });
+
+        navMerchant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent merchantIntent = new Intent(ProfilePage.this, MerchantPage.class);
+                startActivity(merchantIntent);
+            }
+        });
+
+        navforum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle ketika elemen navMerchant diklik
+                Intent forumtIntent = new Intent(ProfilePage.this, ForumPage.class);
+                startActivity(forumtIntent);
+            }
+        });
+
+
+        navarticle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent articleIntent = new Intent(ProfilePage.this, ArticlePage.class);
+                startActivity(articleIntent);
+            }
+        });
+
+
     }
 }
