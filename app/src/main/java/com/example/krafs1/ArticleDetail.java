12d4a0c5491612354612de1d9323a28e9dc13457
amiiin94 <amin942004@gmail.com>
2023-11-20 23:44:42 +0700

    page artikel udah bisa get

---
+++ app/src/main/java/com/example/krafs1/ArticleDetail.java
@@ -0,0 +1,99 @@
+package com.example.krafs1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.NumberFormat;
+import java.util.Currency;
+import java.util.Locale;
+
+public class ArticleDetail extends AppCompatActivity {
+    TextView title, content;
+    ImageView image;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.article_detail_page);
+
+        Intent intent = getIntent();
+        if (intent != null && intent.hasExtra("ARTICLE_ID")) {
+            String articleId = intent.getStringExtra("ARTICLE_ID");
+            Log.d("ArticleDetail", "Received ARTICLE_ID: " + articleId);
+
+            title = findViewById(R.id.title);
+            content = findViewById(R.id.content);
+            image = findViewById(R.id.image);
+
+            getArticleById(articleId);
+        } else {
+            // Penanganan jika ID tidak diterima dengan benar
+            Log.d("ArticleDetail", "ARTICLE_ID not found");
+            Toast.makeText(this, "ID not found", Toast.LENGTH_SHORT).show();
+            finish(); // Anda mungkin ingin menutup aktivitas ini jika ID tidak ditemukan
+        }
+
+    }
+
+    public void getArticleById(String articleId) {
+        String url = "https://ap-southeast-1.aws.data.mongodb-api.com/app/application-0-iyoxv/endpoint/getarticlebyid?id=" + articleId;
+
+        StringRequest stringRequest = new StringRequest(
+                Request.Method.GET,
+                url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONArray articles = new JSONArray(response);
+
+                            if (articles.length() > 0) {
+                                // Assuming you want to get the first article in the array
+                                JSONObject articleJson = articles.getJSONObject(0);
+
+                                String idp = articleJson.getString("_id");
+                                String title = articleJson.getString("title");
+                                String content = articleJson.getString("content");
+
+                                // Taro data di id textview
+                                ArticleDetail.this.title.setText(title);
+                                ArticleDetail.this.content.setText(content);
+                            } else {
+                                // Handle the case where no article is found
+                                Log.d("ArticleDetail", "No article found for ARTICLE_ID: " + articleId);
+                                Toast.makeText(ArticleDetail.this, "Article not found", Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(ArticleDetail.this, error.toString().trim(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        requestQueue.add(stringRequest);
+    }
+}
--- app/src/main/java/com/example/krafs1/ArticlePage.java
+++ app/src/main/java/com/example/krafs1/ArticlePage.java
@@ -6,13 +6,34 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.Currency;
+import java.util.List;
+import java.util.Locale;
 
 public class ArticlePage extends AppCompatActivity {
     private LinearLayout homepage, navmerchant, navforum, navprofile;
 
-
+    private List<Article> articleList;
+    private RecyclerView rv;
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.article_page);
@@ -21,6 +42,15 @@ public class ArticlePage extends AppCompatActivity {
         navmerchant = findViewById(R.id.navmerchant);
         navforum = findViewById(R.id.navforum);
         navprofile = findViewById(R.id.navprofile);
+
+        //RecycleView
+        rv = findViewById(R.id.rv);
+        rv.setLayoutManager(new GridLayoutManager(this, 1));
+        int horizontalSpace = getResources().getDimensionPixelSize(R.dimen.space_between_cards_horizontal);
+        int verticalSpace = getResources().getDimensionPixelSize(R.dimen.space_between_cards_vertical);
+        rv.addItemDecoration(new SpaceItemDecoration(this, horizontalSpace, verticalSpace));
+
+        articleList = new ArrayList<>();
 
         homepage.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,12 +90,85 @@ public class ArticlePage extends AppCompatActivity {
                 }
             }
         });
+
+        getAllArticles();
     }
 
+    public void getAllArticles() {
+        String urlEndPoints = "https://ap-southeast-1.aws.data.mongodb-api.com/app/application-0-iyoxv/endpoint/getAllArticles";
 
+        StringRequest sr = new StringRequest(
+                Request.Method.GET,
+                urlEndPoints,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONArray articles = new JSONArray(response);
+                            for (int i = 0; i < articles.length(); i++) {
+                                JSONObject articleJson = articles.getJSONObject(i);
 
+                                String idp = articleJson.getString("_id");
+                                String title = articleJson.getString("title");
+                                String content = articleJson.getString("content");
+                                String imageUrl = articleJson.getString("img");
 
+                                // Create an Article object and add it to the list
+                                Article article = new Article(idp, title, content, imageUrl);
+                                articleList.add(article);
+                            }
+                            displayArticle();  // Correct method name
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(ArticlePage.this, error.toString().trim(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        requestQueue.add(sr);
+    }
 
+    private void displayArticle() {
+        // ... (existing code)
 
+        // Create an instance of ArticleAdapter
+        ArticleAdapter articleAdapter = new ArticleAdapter(articleList);
+        rv.setAdapter(articleAdapter);  // Set the adapter to the RecyclerView
+    }
 
+    public static class Article {
+        private String idp;
+        private String title;
+        private String content;
+        private String imageUrl;
+
+        public Article(String idp, String title, String content, String imageUrl) {
+            this.idp = idp;
+            this.title = title;
+            this.content = content;
+            this.imageUrl = imageUrl;
+        }
+
+        public String getIdp() {
+            return idp;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public String getContent() {
+            return content;
+        }
+
+        public String getImageUrl() {
+            return imageUrl;
+        }
+    }
 }
